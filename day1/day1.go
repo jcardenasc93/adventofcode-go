d3d1508740ab8be40821d0caf80002d1e6a51265
jcardenasc93 <jcardenasc93@gmail.com>
2022-08-07 20:52:00 -0500

    Add solution for part two (day 1)

--- day1/day1.go
+++ day1/day1.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 )
 
-func checkIncreaseMeasurement() uint16 {
+func checkIncreaseMeasurement1() uint16 {
 	var input string
 	increasesCount := 0
 	lastMeasure := 0
@@ -20,8 +20,7 @@ func checkIncreaseMeasurement() uint16 {
 			break
 		}
 		measure, err := strconv.Atoi(input)
-		if err != nil {
-			log.Println("Error reading measure, is not a number")
+		if checkForError(err) {
 			return 0
 		}
 		if measure > lastMeasure {
@@ -34,7 +33,61 @@ func checkIncreaseMeasurement() uint16 {
 	return uint16(increasesCount)
 }
 
+func checkIncreaseMeasurement2() uint16 {
+	var input string
+	increasesCount := 0
+	lastSum := 0
+	scanner := bufio.NewScanner(os.Stdin)
+	const slidingWindowLen = 3
+	measureSlidingWindow := []int{}
+	for {
+		scanner.Scan()
+		input = scanner.Text()
+		measure, err := strconv.Atoi(input)
+		if len(measureSlidingWindow) < slidingWindowLen {
+			measureSlidingWindow = append(measureSlidingWindow, measure)
+		}
+		if len(measureSlidingWindow) == slidingWindowLen {
+			// Reach max window capacity
+			windowSum := sumArray(measureSlidingWindow...)
+			if windowSum > lastSum && lastSum != 0 {
+				increasesCount += 1
+			}
+			lastSum = windowSum
+			// Free one item from window
+			popFirstElement(&measureSlidingWindow)
+		}
+		if err != nil {
+			break
+		}
+	}
+	return uint16(increasesCount)
+}
+
+func checkForError(err error) bool {
+	if err != nil {
+		log.Println("Error reading measure, is not a number")
+		return true
+	}
+	return false
+}
+
+func sumArray(nums ...int) int {
+	result := 0
+	for _, num := range nums {
+		result += num
+	}
+	return result
+}
+
+func popFirstElement(slice *[]int) {
+	_, *slice = (*slice)[0], (*slice)[1:]
+}
+
 func RunDay1() {
-	fmt.Println("\nPlease enter depht measurement")
-	fmt.Println("Total of increases", checkIncreaseMeasurement())
+	// fmt.Println("Running part one")
+	// fmt.Println("\nPlease enter depht measurement")
+	// fmt.Println("Total of increases", checkIncreaseMeasurement1())
+	fmt.Println("Running part two")
+	fmt.Println("Total of increases", checkIncreaseMeasurement2())
 }
