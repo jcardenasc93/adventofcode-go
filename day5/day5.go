ff81ca53ef5e7169cbf5ecdeed3069caffc6b8d6
jcardenasc93 <jcardenasc93@gmail.com>
2022-11-15 18:29:08 -0500

    Adds solution for first part of day 5

--- day5/day5.go
+++ day5/day5.go
@@ -8,6 +8,9 @@ import (
 	"strings"
 )
 
+var lenX int
+var lenY int
+
 func RunDay5() {
 	readInputFile()
 }
@@ -20,8 +23,10 @@ func checkError(err error) {
 }
 
 type Segment struct {
-	xRange []int
-	yRange []int
+	minX int
+	minY int
+	maxX int
+	maxY int
 }
 
 func getMaxMin(val1 int, val2 int) (int, int) {
@@ -37,46 +42,54 @@ func getMaxMin(val1 int, val2 int) (int, int) {
 	return max, min
 }
 
-func initSegment(ends []string) Segment {
+func isValidSegment(segment []string) (bool, []int, []int) {
+	x1, err := strconv.Atoi(strings.Split(segment[0], ",")[0])
+	checkError(err)
+	x2, err := strconv.Atoi(strings.Split(segment[1], ",")[0])
+	checkError(err)
+
+	// Get max & min of y
+	y1, err := strconv.Atoi(strings.Split(segment[0], ",")[1])
+	checkError(err)
+	y2, err := strconv.Atoi(strings.Split(segment[1], ",")[1])
+	checkError(err)
+
+	return (x1 == x2 || y1 == y2), []int{x1, x2}, []int{y1, y2}
+}
+
+func initSegment(xVals []int, yVals []int) Segment {
 	var minX int
 	var minY int
 	var maxX int
 	var maxY int
 
 	// Get max & min of x
-	x1, err := strconv.Atoi(strings.Split(ends[0], ",")[0])
-	checkError(err)
-	x2, err := strconv.Atoi(strings.Split(ends[1], ",")[0])
-	checkError(err)
-	maxX, minX = getMaxMin(x1, x2)
+	maxX, minX = getMaxMin(xVals[0], xVals[1])
 
 	// Get max & min of y
-	y1, err := strconv.Atoi(strings.Split(ends[0], ",")[1])
-	checkError(err)
-	y2, err := strconv.Atoi(strings.Split(ends[1], ",")[1])
-	checkError(err)
-	maxY, minY = getMaxMin(y1, y2)
-
-	xrange := []int{}
-	yrange := []int{}
-
-	for i := minX; i <= maxX; i++ {
-		xrange = append(xrange, i)
-	}
-
-	for i := minY; i <= maxY; i++ {
-		yrange = append(yrange, i)
-	}
+	maxY, minY = getMaxMin(yVals[0], yVals[1])
 
 	segment := Segment{
-		xRange: xrange,
-		yRange: yrange,
+		minX: minX,
+		minY: minY,
+		maxX: maxX,
+		maxY: maxY,
 	}
+
+	// Update matrix length
+	if maxY > lenY {
+		lenY = maxY + 1
+	}
+	if maxX > lenX {
+		lenX = maxX + 1
+	}
+
 	return segment
 }
 
 func readInputFile() {
-	file, err := os.Open("file.txt")
+	// file, err := os.Open("file.txt.bp")
+	file, err := os.Open("file.txt.bp")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -87,7 +100,41 @@ func readInputFile() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		inputSegment := strings.Split(line, " -> ")
-		segments = append(segments, initSegment(inputSegment))
+		segmentValid, xVals, yVals := isValidSegment(inputSegment)
+		if segmentValid == true {
+			segments = append(segments, initSegment(xVals, yVals))
+		}
 	}
-	fmt.Println(segments)
+	processSegments(segments)
+}
+
+func processSegments(segments []Segment) {
+	fmt.Println(len(segments))
+	fmt.Println(countLines(&segments))
+}
+
+func countLines(lines *[]Segment) int {
+	var count int
+	size, _ := getMaxMin(lenX, lenY)
+	linesMatrix := make([][]int, size)
+	for i := range linesMatrix {
+		linesMatrix[i] = make([]int, size)
+	}
+
+	for _, line := range *lines {
+		for j := line.minY; j <= line.maxY; j++ {
+			for i := line.minX; i <= line.maxX; i++ {
+				linesMatrix[j][i] += 1
+			}
+		}
+	}
+
+	for j, row := range linesMatrix {
+		for i := range row {
+			if linesMatrix[j][i] >= 2 {
+				count += 1
+			}
+		}
+	}
+	return count
 }
