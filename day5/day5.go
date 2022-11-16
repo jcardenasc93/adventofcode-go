0ca0c5132b6034539ac3fe13d6a5e52185f586fc
jcardenasc93 <jcardenasc93@gmail.com>
2022-11-16 00:19:10 -0500

    Adds solution for part two and improves performance avoiding traversal the final matrix again to get the final count

--- day5/day5.go
+++ day5/day5.go
@@ -3,6 +3,7 @@ package day5
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -12,7 +13,7 @@ var lenX int
 var lenY int
 
 func RunDay5() {
-	readInputFile()
+	readInputFile(true)
 }
 
 func checkError(err error) {
@@ -27,6 +28,11 @@ type Segment struct {
 	minY int
 	maxX int
 	maxY int
+}
+
+type Diagonal struct {
+	rangeX []int
+	rangeY []int
 }
 
 func getMaxMin(val1 int, val2 int) (int, int) {
@@ -77,18 +83,71 @@ func initSegment(xVals []int, yVals []int) Segment {
 	}
 
 	// Update matrix length
-	if maxY > lenY {
+	if maxY >= lenY {
 		lenY = maxY + 1
 	}
-	if maxX > lenX {
+	if maxX >= lenX {
 		lenX = maxX + 1
 	}
 
 	return segment
 }
 
-func readInputFile() {
-	// file, err := os.Open("file.txt.bp")
+func initDiagonal(diagonal []string) *Diagonal {
+	x1, err := strconv.Atoi(strings.Split(diagonal[0], ",")[0])
+	checkError(err)
+	x2, err := strconv.Atoi(strings.Split(diagonal[1], ",")[0])
+	checkError(err)
+	y1, err := strconv.Atoi(strings.Split(diagonal[0], ",")[1])
+	checkError(err)
+	y2, err := strconv.Atoi(strings.Split(diagonal[1], ",")[1])
+	checkError(err)
+
+	// Creates range for x
+	var rangex []int
+	if x1 <= x2 {
+		for i := x1; i <= x2; i++ {
+			rangex = append(rangex, i)
+		}
+	} else {
+		for i := x1; i >= x2; i-- {
+			rangex = append(rangex, i)
+		}
+	}
+
+	// Creates range for y
+	var rangey []int
+	if y1 <= y2 {
+		for i := y1; i <= y2; i++ {
+			rangey = append(rangey, i)
+		}
+	} else {
+		for i := y1; i >= y2; i-- {
+			rangey = append(rangey, i)
+		}
+	}
+
+	// Update matrix length
+	maxX, _ := getMaxMin(x1, x2)
+	maxY, _ := getMaxMin(y1, y2)
+	if lenX <= maxX {
+		lenX = maxX + 1
+	}
+	if lenY <= maxY {
+		lenY = maxY + 1
+	}
+
+	if math.Abs(float64(x1)-float64(x2)) == math.Abs(float64(y1)-float64(y2)) {
+		d := Diagonal{
+			rangeX: rangex,
+			rangeY: rangey,
+		}
+		return &d
+	}
+	return nil
+}
+
+func readInputFile(withDiagonals bool) {
 	file, err := os.Open("file.txt.bp")
 	if err != nil {
 		fmt.Println(err)
@@ -96,6 +155,7 @@ func readInputFile() {
 	defer file.Close()
 
 	segments := []Segment{}
+	diagonals := []Diagonal{}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -104,37 +164,62 @@ func readInputFile() {
 		if segmentValid == true {
 			segments = append(segments, initSegment(xVals, yVals))
 		}
+		if withDiagonals == true {
+			diagonal := initDiagonal(inputSegment)
+			if diagonal != nil {
+				diagonals = append(diagonals, *diagonal)
+			}
+		}
 	}
-	processSegments(segments)
+	processSegments(segments, diagonals, true)
 }
 
-func processSegments(segments []Segment) {
-	fmt.Println(len(segments))
-	fmt.Println(countLines(&segments))
-}
-
-func countLines(lines *[]Segment) int {
-	var count int
+func processSegments(segments []Segment, diagonals []Diagonal, withDiagonals bool) {
+	count := 0
 	size, _ := getMaxMin(lenX, lenY)
 	linesMatrix := make([][]int, size)
 	for i := range linesMatrix {
 		linesMatrix[i] = make([]int, size)
 	}
+	processLines(&segments, &linesMatrix, &count)
+	if withDiagonals == true {
+		processDiagonals(&diagonals, &linesMatrix, &count)
+	}
+	fmt.Println(count)
+}
 
-	for _, line := range *lines {
+func processLines(segments *[]Segment, matrix *[][]int, count *int) {
+	for _, line := range *segments {
 		for j := line.minY; j <= line.maxY; j++ {
 			for i := line.minX; i <= line.maxX; i++ {
-				linesMatrix[j][i] += 1
+				(*matrix)[j][i] += 1
+				if (*matrix)[j][i] == 2 {
+					*count += 1
+				}
 			}
 		}
 	}
-
-	for j, row := range linesMatrix {
-		for i := range row {
-			if linesMatrix[j][i] >= 2 {
-				count += 1
-			}
-		}
-	}
-	return count
 }
+
+func processDiagonals(diagonals *[]Diagonal, matrix *[][]int, count *int) {
+	for _, diagonal := range *diagonals {
+		for k := range diagonal.rangeY {
+			i := diagonal.rangeX[k]
+			j := diagonal.rangeY[k]
+			(*matrix)[j][i] += 1
+			if (*matrix)[j][i] == 2 {
+				*count += 1
+			}
+		}
+	}
+}
+
+// func countLines(matrix *[][]int) int {
+// 	var count int
+// 	for j, row := range *matrix {
+// 		for i := range row {
+// 			if (*matrix)[j][i] >= 2 {
+// 				count += 1
+// 			}
+// 		}
+// 	}
