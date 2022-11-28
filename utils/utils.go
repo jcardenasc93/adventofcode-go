53b89d2026ad12b8e38eedeba723adc48d875355
jcardenasc93 <jcardenasc93@gmail.com>
2022-11-27 21:57:01 -0500

    Adds utils funcs to parse str to int and slices utils like check if item exists or del an item

--- utils/utils.go
+++ utils/utils.go
@@ -23,3 +23,29 @@ func ParseStringToIntSlice(input string) []int {
 	}
 	return output
 }
+
+func ParseStrInt(number string) int {
+	num, err := strconv.Atoi(number)
+	CheckError(err)
+	return num
+}
+
+func IsItemIn[T comparable](slice []T, item T) bool {
+	for _, i := range slice {
+		if item == i {
+			return true
+		}
+	}
+	return false
+}
+
+func DelItem[T comparable](slice []T, item T) []T {
+	resp := []T{}
+	for j, i := range slice {
+		if item == i {
+			resp = append(resp, slice[:j]...)
+			resp = append(resp, slice[j+1:]...)
+		}
+	}
+	return resp
+}
