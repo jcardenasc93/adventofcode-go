a554422290cf749db07a99a03e3148fc07270fed
jcardenasc93 <jcardenasc93@gmail.com>
2023-01-26 19:44:02 -0500

    Adds solution for part two day 15 (Chiton)

--- day15/day15.go
+++ day15/day15.go
@@ -27,6 +27,7 @@ func readInputFile() {
 		}
 	}
 	findLowerRiskPath(&riskMap)
+	findLowerRisk5x5(&riskMap)
 }
 
 func processLine(line string, riskMap *[][]int) {
@@ -48,8 +49,6 @@ func findLowerRiskPath(riskMap *[][]int) {
 		{0, -1}, // left
 	}
 
-	risks := map[string]int{}
-
 	item := &utils.Item{
 		Value:    "0,0",
 		Priority: 0,
@@ -63,11 +62,10 @@ func findLowerRiskPath(riskMap *[][]int) {
 			continue
 		}
 		visited = append(visited, coords.Value)
-		risks[coords.Value] = coords.Priority
 		y, x := getCoordsFromStr(coords.Value)
 
 		if y == len(*riskMap)-1 && x == len(*riskMap)-1 {
-			fmt.Println(risks[coords.Value])
+			fmt.Println(coords.Priority)
 			return
 		}
 
@@ -79,7 +77,6 @@ func findLowerRiskPath(riskMap *[][]int) {
 					nextStep := &utils.Item{
 						Value:    fmt.Sprintf("%d,%d", y_, x_),
 						Priority: risk,
-						// Index:    pq.Len(),
 					}
 					heap.Push(&pq, nextStep)
 				}
@@ -93,4 +90,79 @@ func getCoordsFromStr(s string) (int, int) {
 	y := utils.ParseStrInt(split[0])
 	x := utils.ParseStrInt(split[1])
 	return y, x
+}
+
+func findLowerRisk5x5(riskMap *[][]int) {
+	pq := make(utils.PriorityQueue, 0)
+	visited := map[string]bool{}
+	directions := [][]int{
+		{-1, 0}, // top
+		{0, 1},  // right
+		{1, 0},  // bottom
+		{0, -1}, // left
+	}
+	projectedSize := len(*riskMap) * 5
+
+	projectedMap := make([][]int, projectedSize)
+	for y := 0; y < len(projectedMap); y++ {
+		projectedMap[y] = make([]int, projectedSize)
+	}
+
+	for y := 0; y < len(projectedMap); y++ {
+		for x := 0; x < len(projectedMap); x++ {
+			if y < len(*riskMap) && x < len(*riskMap) {
+				projectedMap[y][x] = (*riskMap)[y][x]
+				continue
+			}
+			projectedMap[y][x] = calcProjectedRisk(x, y, riskMap)
+		}
+	}
+
+	item := &utils.Item{
+		Value:    "0,0",
+		Priority: 0,
+		Index:    0,
+	}
+	heap.Push(&pq, item)
+
+	for pq.Len() > 0 {
+		coords := heap.Pop(&pq).(*utils.Item)
+		_, ok := visited[coords.Value]
+
+		if ok {
+			continue
+		}
+
+		visited[coords.Value] = true
+		y, x := getCoordsFromStr(coords.Value)
+
+		if y == projectedSize-1 && x == projectedSize-1 {
+			fmt.Println(coords.Priority)
+			return
+		}
+
+		for _, dir := range directions {
+			y_, x_ := y+dir[0], x+dir[1]
+			nextCoord := fmt.Sprintf("%d,%d", y_, x_)
+			_, ok = visited[nextCoord]
+			if !ok {
+				if y_ >= 0 && y_ < projectedSize && x_ >= 0 && x_ < projectedSize {
+					projectedRisk := projectedMap[y_][x_]
+					risk := coords.Priority + projectedRisk
+					nextStep := &utils.Item{
+						Value:    fmt.Sprintf("%d,%d", y_, x_),
+						Priority: risk,
+					}
+					heap.Push(&pq, nextStep)
+				}
+			}
+		}
+	}
+}
+
+func calcProjectedRisk(x, y int, rm *[][]int) (risk int) {
+	mod := len(*rm)
+	risk = (*rm)[y%mod][x%mod] + y/mod + x/mod
+
+	return (risk-1)%9 + 1
 }
